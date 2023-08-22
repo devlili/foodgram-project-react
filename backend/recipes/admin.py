1582f32421cea8fb2ff62bcc1ab61cf352e8c7c8
Лилия Альмухаметова <almuhametova@gmail.com>
2023-08-23 01:37:51 +0500

    Pep8

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -30,13 +30,14 @@ class RecipeIngredientInline(admin.TabularInline):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name", "author", 'in_favorites')
+    list_display = ("pk", "name", "author", "in_favorites")
     list_filter = ("author", "name", "tags")
     inlines = [RecipeIngredientInline]
 
-    @display(description='Количество в избранных')
+    @display(description="Количество в избранных")
     def in_favorites(self, obj):
         return obj.favorites.count()
+
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
